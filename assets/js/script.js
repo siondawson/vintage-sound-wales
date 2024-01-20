172262a8f166b7abfede7233a613a2417b23b264
siondawson <siondawson91@gmail.com>
2024-01-20 21:44:23 +0000

    fixed js and css links

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function(){
+    var typed = new Typed('.typed', {
+      strings: ["Repairs", "Restorations", "Sales", "Acquisitions"],
+      typeSpeed: 50, // typing speed in milliseconds
+      backSpeed: 30, // backspacing speed in milliseconds
+      backDelay: 1000, // delay before starting to backspace
+      startDelay: 500, // delay to start typing after initialization
+      loop: true, // loop through the items
+    });
+  });
